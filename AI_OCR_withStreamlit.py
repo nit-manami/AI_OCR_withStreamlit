d57400eea9c3cec941cd271d54e63c789b0fc861
nit-manami <manami.sakurai@niterragroup.com>
2024-09-20 08:44:50 +0900

    Root

---
+++ AI_OCR_withStreamlit.py
@@ -0,0 +1,20 @@
+import numpy as np 
+from PIL import Image, ImageDraw
+import easyocr
+import streamlit as st
+
+reader = easyocr.Reader(['ja','en'])
+selected_image = st.file_uploader('upload image', type='jpg')
+
+original_image = st.empty()
+result_image = st.empty()
+
+if (selected_image != None):
+    original_image.image(selected_image)
+    pil = Image.open(selected_image)
+    result = reader.readtext(np.array(pil))
+    draw = ImageDraw.Draw(pil)
+    for each_result in result:
+        draw.rectangle(tuple(each_result[0][0] + each_result[0][2]), outline=(0, 0, 255), width=3)
+        st.write(each_result[1])
+    result_image.image(pil)
